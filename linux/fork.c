fe0f32217107419b13c1d5139434ce03b54c4105
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-04-03 20:50:04 +0900

    Update fork.c

--- linux/fork.c
+++ linux/fork.c
@@ -1,7 +1,7 @@
-#include <unistd.h> // #include <io.h>
+#include <unistd.h> 		// #include <io.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <err.h>	// #include <errhandlingapi.h>
+#include <err.h>		// #include <errhandlingapi.h>
 
 static void child() {
 	printf("I'm child! my pid is %d.\n", getpid());
